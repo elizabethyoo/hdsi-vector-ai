6c178ea8f2ce2b2221bb1d269f25f78a6bb494b9
Elizabeth C. Graff <yoo.elizabeth.c@gmail.com>
2024-09-25 21:08:38 -0400

    select functions to apply to virscan data

--- scripts/peptides.R
+++ scripts/peptides.R
@@ -1,6 +1,6 @@
 library(Peptides)
 library(readxl)
-library(readr)
+library(tidyverse)
 
 # set working directory to where script is
 current_file_dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
@@ -18,14 +18,28 @@ cor_lib <- read_csv(file.path(shrock_data_dir,
                               "Shrock_2020_Coronavirus-library.csv"))
 vir_lib <- read_csv(file.path(shrock_data_dir,
                               "Shrock_2020_VirScan-library.csv"))
+dim(which(is.na(vir_lib)))
 
 
-# subset of virscan library whose peptides were detected in samples 
-vir_lib_subset <- vir_lib[match(rownames(vir_z), vir_lib$id), ]
-test <- vir_lib_subset[1, ]
-test_pep <- test$peptide
+# subset of virscan library whose peptides were detected in samples
+vir_lib_subset <- vir_lib[vir_lib$id %in% vir_z$id, ]
 
-# test some functions of peptides package on a test peptide sequence 
+# list of peptides functions that take AA sequences as arguments
+# and whose outputs might be useful for downstream analysis
+# see https://github.com/dosorio/Peptides/tree/master for descriptions
+pep_funcs <- c("aaComp", "aIndex")
+# Apply each function to the 'peptide' column of vir_lib_subset and save the outputs into a new dataframe
+results <- sapply(pep_funcs, function(func) {
+    do.call(func, list(vir_lib_subset$peptide))
+})
 
+# Convert the results to a dataframe
+results_df <- as.data.frame(results)
 
-# Q: the peptides package has a variety of features -- which are the most relevant? 
+# Set the column names to match the function names
+colnames(results_df) <- pep_funcs
+
+# Print the resulting dataframe
+print(results_df)
+
+# ... sowhat  
